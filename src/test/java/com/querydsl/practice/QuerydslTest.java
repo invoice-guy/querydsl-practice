ecfebe0b54405eae60ca976bdef67c5382cea25e
invoice-guy <chihoon82@hanmail.net>
2021-05-03 04:51:42 +0900

    20210503 045100

--- src/test/java/com/querydsl/practice/QuerydslTest.java
+++ src/test/java/com/querydsl/practice/QuerydslTest.java
@@ -3,6 +3,7 @@ package com.querydsl.practice;
 import com.querydsl.practice.domain.*;
 
 import com.querydsl.practice.dto.SvcCust;
+import com.querydsl.practice.dto.SvcProd;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,12 @@ public class QuerydslTest {
 
     @Autowired
     private CustRepositorySupport custRepositorySupport;
+
+    @Autowired
+    private ProdRepository prodRepository;
+
+    @Autowired
+    private ProdRepositorySupport prodRepositorySupport;
 
     @Test
     public void select_svcmgmtnum_by_svcnum() {
@@ -87,6 +94,48 @@ public class QuerydslTest {
 
         assertThat(result.size(), is(1));
         assertThat(result.get(0).getCustnm(), is("sookyung"));
+    }
+
+    @Test
+    public void insert_prod_by_svcnum() {
+        svcRepository.saveAll(Arrays.asList(
+                new Svc("70000","01028268646","80000"),
+                new Svc("70001","01010005134","80001"),
+                new Svc("70002","01091245875","80002")
+        ));
+
+        custRepository.saveAll(Arrays.asList(
+                new Cust("80000","chihoon"),
+                new Cust("80001","insoo"),
+                new Cust("80002","sookyung")
+        ));
+
+        List<Svc> result = svcRepositorySupport.findBySvcnum("01028268646");
+
+        //then
+        assertThat(result.size(), is(1));
+        assertThat(result.get(0).getSvcmgmtnum(), is("70000"));
+
+        String svcmgmtnum=result.get(0).getSvcmgmtnum();
+
+        System.out.println("get svcmgmt_num = " + svcmgmtnum);
+
+        prodRepository.save(new Prod(svcmgmtnum, "99991231235959", "20210503090000", "NA00001600" , "20210503", "99991231"));
+        prodRepository.save(new Prod(svcmgmtnum, "99991231235959", "20210503090000", "NA00002500" , "20210503", "99991231"));
+
+        List<Prod> result2 = prodRepositorySupport.findProdBySvcmgmtnum("70000");
+
+        assertThat(result2.size(), is(2));
+        assertThat(result2.get(0).getProdid(), is("NA00001600"));
+        assertThat(result2.get(1).getProdid(), is("NA00002500"));
+
+        List<SvcProd> result3 = prodRepositorySupport.findProdBySvcnum("01028268646");
+
+        assertThat(result3.size(), is(2));
+        assertThat(result3.get(0).getProdid(), is("NA00001600"));
+        assertThat(result3.get(1).getProdid(), is("NA00002500"));
+
+        assertThat(result3.get(0).getEffenddtm(), is("99991231235959"));
     }
 
 }
